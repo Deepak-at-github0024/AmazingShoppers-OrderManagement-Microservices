8f390b0603c230b104728dca921e1c6f57a1129c
Deepak Pathak <kumardk0024@gmail.com>
2026-07-09 07:46:05 +0530

    Implement API Gatway routing with Eureka

---
+++ order-service/src/main/java/com/example/order_service/Controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.example.order_service.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Order Service is running";
+    }
+}
